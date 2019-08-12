d52005f9a4e2fc15639f8f462037fe95457a6343
Vítor Cunha <vitorrcunhaa@gmail.com>
2019-08-12 16:15:38 -0300

    Add user registration

--- caleta_test/urls.py
+++ caleta_test/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
+from billing import views
 
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('register/', views.register, name='register'),
     path('admin/', admin.site.urls),
     path('billing/', include('billing.urls'), name='billing'),
 ]
